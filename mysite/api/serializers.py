fb1575b47edb0201623b1ffae3aacbb2937213e9
ngocduong777 <baongocduong1@gmail.com>
2022-05-02 22:03:08 +0700

    Added my project

---
+++ mysite/api/serializers.py
@@ -0,0 +1,11 @@
+#change Python code to JSON etc...
+from rest_framework import serializers
+from .models import Room 
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id','code','host','guest_can_pause','votes_to_skip','created_at')
+        
+
